c40cc7420e232579d0aba65a776cf5be4db1ccd2
amunoz124 <and.munoz41@gmail.com>
2023-04-07 20:46:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/disneycharacterapp/DisneyAdapter.kt
@@ -0,0 +1,44 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.disneycharacterapp.R
+
+class DisneyAdapter(private val disneyList: List<String>) : RecyclerView.Adapter<DisneyAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        val disneyImage: ImageView
+        val disneyName : TextView
+        val disneyFilms : TextView
+
+        init {
+            // Find our RecyclerView item's ImageView for future use
+            disneyImage = view.findViewById(R.id.disney_image)
+            disneyName = view.findViewById(R.id.name)
+            disneyFilms = view.findViewById(R.id.films)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DisneyAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.disney_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DisneyAdapter.ViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(disneyList[position])
+            .centerCrop()
+            .into(holder.disneyImage)
+
+//        holder.disneyName.text = "Character Name: ${name}"
+//        holder.disneyFilms.text = "Films: ${films}"
+    }
+
+    override fun getItemCount() = disneyList.size
+}
---
+++ app/src/main/java/com/example/disneycharacterapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.disneycharacterapp
+
+import DisneyAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var disneyList: MutableList<String>
+    private lateinit var rvDisney: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvDisney = findViewById(R.id.disney_list)
+        disneyList = mutableListOf()
+
+        getDisneyImageURL()
+    }
+
+    private fun getDisneyImageURL() {
+
+        val client = AsyncHttpClient()
+        val url = "https://api.disneyapi.dev/characters"
+        client[url, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+
+                val charactersArray = json?.jsonObject?.getJSONArray("data")
+
+                for (i in 0 until (charactersArray?.length() ?: 0)) {
+                    val characterObject = charactersArray?.getJSONObject(i)
+                    val imageUrl = characterObject?.getString("imageUrl")
+//                    val name = characterObject?.getString("name")
+//                    val films = characterObject?.getJSONArray("films").toString()
+                    if (imageUrl != null) {
+                        disneyList.add(imageUrl)
+                    }
+                }
+
+                val adapter = DisneyAdapter(disneyList)
+                rvDisney.adapter = adapter
+                rvDisney.layoutManager = LinearLayoutManager(this@MainActivity)
+                rvDisney.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("API Error", errorResponse)
+            }
+
+        }]
+
+    }
+}
